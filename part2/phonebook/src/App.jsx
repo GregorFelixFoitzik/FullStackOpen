7b2cae7b81fb1dca8427d6f2e05d276080f21a3b
Gregor Foitzik <greg.foitzik@gmail.com>
2024-11-01 14:44:58 +0200

    part2 exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,17 +2,27 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567', 
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+
+  // Phonebook Handling
   const [newName, setNewName] = useState(
     'adding new name'
   )
+  const handlePersonsChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   const [newNumber, setNewNumber] = useState(
     'adding new number'
   )
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
 
   const addName = (event) => {
     event.preventDefault()
@@ -32,14 +42,8 @@ const App = () => {
     }  
   }
 
-  const handlePersonsChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
 
-
+  // Displaying Phonebook
   const DisplayPhonebook = ({persons}) => {
     return (
       <>
@@ -49,9 +53,27 @@ const App = () => {
   }
   const DisplayPerson = ({person}) => <div>{person.name} {person.number}</div>
 
+
+  // Filter 
+  const [showAll, setShowAll] = useState(
+    ''
+  )
+
+  const handlePersonsFilter = (event) => {
+    setShowAll(event.target.value)
+  }
+
+  // condition ? true : false
+  const personsToShow = (showAll===true) ? persons : persons.filter(person => person.name.includes(showAll))
+
+  // App
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with: <input value={showAll} onChange={handlePersonsFilter}/>
+        </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handlePersonsChange}/>
@@ -64,7 +86,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <DisplayPhonebook persons={persons} />
+      <DisplayPhonebook persons={personsToShow} />
     </div>
   )
 }
