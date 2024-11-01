61596fcb5d616a93e028fca7bcd8d479453356a5
Gregor Foitzik <greg.foitzik@gmail.com>
2024-11-01 13:58:15 +0200

    part2 exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,29 +1,44 @@
 import { useState } from 'react'
+import { regexFinishNumbers } from './components/regexNumber'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '040-1234567', 
+    }
   ]) 
   const [newName, setNewName] = useState(
     'adding new name'
   )
+  const [newNumber, setNewNumber] = useState(
+    'adding new number'
+  )
 
   const addName = (event) => {
     event.preventDefault()
-    const noteObject = {
+    const nameObject = {
       name: newName,
+      number: newNumber,
       id: String(persons.length + 1),
     }
-  
-    setPersons(persons.concat(noteObject))
-    setNewName('')
+    
+    const onlyNames =  persons.map(person => person.name)
+    // https://stackoverflow.com/a/56474783
+    if (onlyNames.includes(nameObject.name)) {
+      alert(`${nameObject.name} is already added to phonebook.`)
+    } else { 
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+    }  
   }
 
   const handlePersonsChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
-
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
 
 
   const DisplayPhonebook = ({persons}) => {
@@ -33,7 +48,7 @@ const App = () => {
       </>
     )
   }
-  const DisplayPerson = ({person}) => <div>{person.name}</div>
+  const DisplayPerson = ({person}) => <div>{person.name} {person.number}</div>
 
   return (
     <div>
@@ -41,6 +56,9 @@ const App = () => {
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handlePersonsChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
