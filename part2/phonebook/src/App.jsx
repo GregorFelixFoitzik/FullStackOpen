4547653770601c4854bd700cfeb4182fac170aa5
Gregor Foitzik <greg.foitzik@gmail.com>
2024-11-05 14:35:25 +0200

    part2 exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect, useInsertionEffect } from 'react'
+import axios from 'axios'
 import { Filter, AddingForm, DisplayPhonebook } from './components/htmlComponents'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+  useEffect(hook, [])
 
   // Phonebook Handling
-  const [newName, setNewName] = useState(
-    'adding new name'
-  )
+  const [newName, setNewName] = useState('adding new name')
   const handlePersonsChange = (event) => {
     setNewName(event.target.value)
   }
 
-  const [newNumber, setNewNumber] = useState(
-    'adding new number'
-  )
+  const [newNumber, setNewNumber] = useState('adding new number')
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
